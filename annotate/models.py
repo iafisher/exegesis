5922d9aec35daacc762957a3e37b3b702215f77a
Ian Fisher <iafisher@protonmail.com>
2018-07-27 21:00:02 -0400

    Add models

--- annotate/models.py
+++ annotate/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Snippet(models.Model):
+    text = models.TextField()
+    path = models.CharField(max_length=500)
+
+
+class Comment(models.Model):
+    created = models.DateField(auto_now_add=True)
+    last_updated = models.DateField(auto_now=True)
+    text = models.TextField()
+    snippet = models.ForeignKey(Snippet, on_delete=models.CASCADE)
