6fa8e40b07c6bc4d0da28d0022e23a5342a33d7a
Ian Fisher <iafisher@protonmail.com>
2018-08-15 13:17:27 -0400

    Add a test for directory and snippet page templates

--- annotate/tests.py
+++ annotate/tests.py
@@ -2,7 +2,7 @@ from django.contrib.auth import get_user_model
 from django.http import HttpRequest
 from django.test import TestCase
 from django.urls import resolve
-from . import views
+from . import models, views
 
 
 class TestURLs(TestCase):
@@ -48,3 +48,20 @@ class TestPages(TestCase):
     def test_login_page_template(self):
         response = self.client.get('/login')
         self.assertTemplateUsed(response, 'annotate/login.html')
+
+    def test_path_page_template(self):
+        self.client.login(username='temporary', password='pwd')
+
+        # Create a minimal project with a directory and a file.
+        project = models.Project.objects.create(name='temporary')
+        models.Directory.objects.create(fullpath='', dirpath='', name='',
+            project=project)
+        models.Snippet.objects.create(fullpath='README.txt', dirpath='',
+            name='README.txt', text='Lorem ipsum', project=project,
+            downloaded=True)
+
+        response = self.client.get('/project/temporary')
+        self.assertTemplateUsed(response, 'annotate/directory.html')
+
+        response = self.client.get('/project/temporary/README.txt')
+        self.assertTemplateUsed(response, 'annotate/snippet.html')
